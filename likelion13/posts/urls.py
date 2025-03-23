4ad8575430559a2a200a1058eb586218ded61eff
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-03-23 19:16:17 +0900

    Fix: 3주차 django 실습

---
+++ likelion13/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from posts.views import *
+
+urlpatterns = [
+    path('', hello_world, name = 'hello_world'),
+    path('page', index, name = 'my-page'),
+]
---
+++ likelion13/posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import JsonResponse # 추가 
+from django.shortcuts import get_object_or_404 # 추가
+
+# Create your views here.
+
+def hello_world(request):
+    if request.method == "GET":
+        return JsonResponse({
+            'status' : 200,
+            'data' : "Hello likelion-13th!"
+        })
+    
+def index(request):
+    return render(request, 'index.html')
