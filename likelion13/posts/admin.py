5bae7fb8c85c2f7f2075026e721df9a2c8cc56e5
Ubuntu <ubuntu@ip-172-31-43-121.ap-northeast-2.compute.internal>
2025-04-11 09:12:47 +0000

    chore: 불필요한 파일 삭제

--- likelion13/posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Comment, Category, PostCategory
-
-admin.site.register(Category) # Category 모델을 admin에 등록
-admin.site.register(Comment)
-admin.site.register(Post) # Post 모델을 admin에 등록
-admin.site.register(PostCategory) # PostCategory 모델을 admin에 등록
