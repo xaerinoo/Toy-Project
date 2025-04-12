24ca9434593393f80f25951a5fe192ba056b9aa7
Ubuntu <ubuntu@ip-172-31-44-28.ap-northeast-2.compute.internal>
2025-04-12 11:34:04 +0000

    settings.py cors 수정

--- toyproject/config/settings.py
+++ toyproject/config/settings.py
@@ -159,4 +159,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
